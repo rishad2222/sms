b259f34f38e315ed9102c257dabd732cfe7a91be
Zahidul Kabir <rihad.mahin803@gmail.com>
2022-10-05 21:48:00 +0600

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Portcity Convocation Sucks
                 </div>
 
                 <div class="links">

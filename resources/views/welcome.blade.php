75926b788f728496c766f8b7de6953c57913729a
Zahidul Kabir <rihad.mahin803@gmail.com>
2022-10-05 21:50:17 +0600

    Banner Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Portcity Convocation Sucks
+                     Isfak is boss
                 </div>
 
                 <div class="links">

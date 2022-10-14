0a6db0bc1be3a2cbf6e9052d6e7e1ae6428f1626
Zahidul Kabir <rihad.mahin803@gmail.com>
2022-10-14 17:43:19 +0600

    SMS Complete

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -8,63 +8,78 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Student Management System</title>
   </head>
   <body>
 
+    @include('navbar')
+
+    <div class="row header-container justify-content-center">
+        <div class="header">
+            <h1>Student Management System</h1>
+        </div>
+    </div>
+
     {{-- Index Function --}}
     @if($layout == 'index')
 
-        <div class="container-fluid">
-            <div class="row">
-                <section class="col">
+        <div class="container-fluid mt-4">
+            <div class="row justify-content-center">
+                <section class="col-md-7">
                     @include('studentlist')
-                </section>
-                <section class="col">
-
                 </section>
             </div>
         </div>
     {{-- Create Function --}}
     @elseif($layout == 'create')
-    <div class="container-fluid">
+    <div class="container-fluid mt-4">
         <div class="row">
             <section class="col">
                 @include('studentlist')
             </section>
             <section class="col">
-                <form action="{{ url('/store') }}" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label>CNE</label>
-                        <input name="cne" type="text" class="form-control" placeholder="Enter CNE">
+
+                <div class="card mb-3">
+                    <img height="453" width="916" class="card-img-top" src="https://www.onatlas.com/wp-content/uploads/2019/03/education-students-people-knowledge-concept-P6MBQ5W-1080x675.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Enter the New Student Information</p>
+
+                      <form action="{{ url('/store') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label>CNE</label>
+                            <input name="cne" type="text" class="form-control" placeholder="Enter CNE">
+                        </div>
+                        <div class="form-group">
+                            <label>First Name</label>
+                            <input name="firstname" type="text" class="form-control" placeholder="Enter First Name">
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name</label>
+                            <input name="lastname" type="text" class="form-control" placeholder="Enter Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label>Age</label>
+                            <input name="age" type="text" class="form-control" placeholder="Enter Age">
+                        </div>
+                        <div class="form-group">
+                            <label>Speciality</label>
+                            <input name="speciality" type="text" class="form-control" placeholder="Enter Speciality">
+                        </div>
+                        <input type="submit" class="btn btn-info" value="Save">
+                        <input type="reset" class="btn btn-warning" value="Reset">
+                    </form>
                     </div>
-                    <div class="form-group">
-                        <label>First Name</label>
-                        <input name="firstname" type="text" class="form-control" placeholder="Enter First Name">
-                    </div>
-                    <div class="form-group">
-                        <label>Last Name</label>
-                        <input name="lastname" type="text" class="form-control" placeholder="Enter Lastname">
-                    </div>
-                    <div class="form-group">
-                        <label>Age</label>
-                        <input name="age" type="text" class="form-control" placeholder="Enter Age">
-                    </div>
-                    <div class="form-group">
-                        <label>Speciality</label>
-                        <input name="speciality" type="text" class="form-control" placeholder="Enter Speciality">
-                    </div>
-                    <input type="submit" class="btn btn-info" value="Save">
-                    <input type="reset" class="btn btn-warning" value="Reset">
-                </form>
+                  </div>
+
+
             </section>
         </div>
     </div>
     {{-- Show Function --}}
     @elseif($layout == 'show')
-    <div class="container-fluid">
+    <div class="container-fluid mt-4">
         <div class="row">
             <section class="col">
                 @include('studentlist')
@@ -76,19 +91,53 @@
     </div>
     {{-- Edit Function --}}
     @elseif($layout == 'edit')
-    <div class="container-fluid">
+    <div class="container-fluid mt-4">
         <div class="row">
             <section class="col">
                 @include('studentlist')
             </section>
             <section class="col">
+                <div class="card mb-3">
+                    <img height="453" width="916" class="card-img-top" src="https://www.onatlas.com/wp-content/uploads/2019/03/education-students-people-knowledge-concept-P6MBQ5W-1080x675.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Update Student Information</p>
 
+                        <form action="{{ url('/update/'.$student->id) }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label>CNE</label>
+                                <input value="{{ $student->cne }}" name="cne" type="text" class="form-control" placeholder="Enter CNE">
+                            </div>
+                            <div class="form-group">
+                                <label>First Name</label>
+                                <input value="{{ $student->firstname }}" name="firstname" type="text" class="form-control" placeholder="Enter First Name">
+                            </div>
+                            <div class="form-group">
+                                <label>Last Name</label>
+                                <input value="{{ $student->lastname }}" name="lastname" type="text" class="form-control" placeholder="Enter Lastname">
+                            </div>
+                            <div class="form-group">
+                                <label>Age</label>
+                                <input value="{{ $student->age }}" name="age" type="text" class="form-control" placeholder="Enter Age">
+                            </div>
+                            <div class="form-group">
+                                <label>Speciality</label>
+                                <input value="{{ $student->speciality }}" name="speciality" type="text" class="form-control" placeholder="Enter Speciality">
+                            </div>
+                            <input type="submit" class="btn btn-info" value="Update">
+                            <input type="reset" class="btn btn-warning" value="Reset">
+                        </form>
+                    </div>
+                </div>
             </section>
         </div>
     </div>
 
     @endif
 
+    <footer>
+
+    </footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- resources/views/studentlist.blade.php
+++ resources/views/studentlist.blade.php
@@ -1,30 +1,42 @@
-<table class="table">
-    <thead class="thead-light">
-      <tr>
-        <th scope="col">CNE</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Age</th>
-        <th scope="col">Speciality</th>
-        <th scope="col">Operations</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach ($students as $student)
-    <tr>
-        <th>{{ $student->cne }}</th>
-        <th>{{ $student->firstname }}</th>
-        <th>{{ $student->lastname }}</th>
-        <th>{{ $student->age }}</th>
-        <th>{{ $student->speciality }}</th>
-        <td>
-            <a href="#" class="btn btn-sm btn-info">Show</a>
-            <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Show</a>
-            <a href="#" class="btn btn-sm btn-danger">Delete</a>
-        </td>
+<div class="card mb-3">
+    <img class="card-img-top" src="https://www.missingkids.org/content/ncmec/en/education/_jcr_content/mainpar/container_1757940280/null/twovariantscolumn/par_1/image.img.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">List of Student</h5>
+      <p class="card-text">You can find here all the Information about Students in the system </p>
+    </div>
 
-    </tr>
-    @endforeach
+    <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">CNE</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Speciality</th>
+            <th scope="col">Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($students as $student)
+        <tr>
+            <th>{{ $student->cne }}</th>
+            <th>{{ $student->firstname }}</th>
+            <th>{{ $student->lastname }}</th>
+            <th>{{ $student->age }}</th>
+            <th>{{ $student->speciality }}</th>
+            <td>
 
-    </tbody>
-  </table>
+                <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+
+            </td>
+
+        </tr>
+        @endforeach
+
+        </tbody>
+      </table>
+  </div>
+
+
+
+
